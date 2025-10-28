5da005745ffc129fb4c20546e4db1ff14f54ed9c
AbijithSebastian <abijithseban14@gmail.com>
2025-10-28 20:37:00 +0530

    Add files via upload

---
+++ S7 -  CD LAB/EXP 7 - FIRST AND FOLLOW/exp7.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define MAX 20
+
+char nonterminals[MAX];
+char productions[MAX][MAX];
+
+int n;
+int ntcnt=0;
+
+void add(char res[],char c){
+    if(!strchr(res,c)){
+        int len=strlen(res);
+        res[len]=c;
+        res[len+1]='\0';
+    }
+}
+
+void findFirst(char res[],char c){
+    if(!isupper(c)){
+        add(res,c);
+        return;
+    }
+    for(int i=0;i<n;i++){
+        if(productions[i][0]==c){ //check
+            int j=3;
+            int eflag=1;
+            while(productions[i][j] && eflag){
+                char temp[MAX]="";
+                findFirst(temp,productions[i][j]);
+                for(int k=0;temp[k]!='\0';k++){
+                    if(temp[k]!='#') add(res,temp[k]);
+                }
+                eflag=strchr(temp,'#')!=NULL;
+                j++;
+            }
+            if(eflag) add(res,'#');
+        }
+    }
+}
+
+void findFollow(char res[],char c){
+    if(productions[0][0]==c){
+        add(res,'$');
+    }
+    for(int i=0;i<n;i++){
+        for(int j=3;productions[i][j]!='\0';j++){
+            if(productions[i][j]==c){ //check
+                int k=j+1;
+                int eflag=1;
+                while(productions[i][k] && eflag){
+                    char temp[MAX]="";
+                    findFirst(temp,productions[i][k]);  //check
+                    for(int m=0;temp[m]!='\0';m++){
+                        if(temp[m]!='#') add(res,temp[m]);
+                    }
+                    eflag=strchr(temp,'#')!=NULL;
+                    k++;
+                }
+                
+                if(eflag && productions[i][0]!=c){
+                    char temp[MAX]="";
+                    findFollow(temp,productions[i][0]); 
+                    for(int m=0;temp[m]!='\0';m++){
+                        add(res,temp[m]);          //check
+                    }
+                }
+            }
+        }
+    }
+}
+
+void main(){
+    printf("Enter no.of productions:");
+    scanf("%d",&n);
+    getchar();
+    printf("Enter the productions:\n");
+    for(int i=0;i<n;i++){
+        printf("Production %d:",i+1);
+        scanf("%s",productions[i]);
+        if(!strchr(nonterminals,productions[i][0])){
+            nonterminals[ntcnt++]=productions[i][0];
+        }
+    }
+    printf("\nFIRST SET:\n");
+    for(int i=0;i<ntcnt;i++){
+        char res[MAX]="";
+        findFirst(res,nonterminals[i]);
+        printf("FIRST(%c)={",nonterminals[i]);
+        for(int j=0;res[j]!='\0';j++){
+            printf(" %c",res[j]);
+        }
+        printf("}\n");
+    }
+    printf("\nFOLLOW SET:\n");
+    for(int i=0;i<ntcnt;i++){
+        char res[MAX]="";
+        findFollow(res,nonterminals[i]);
+        printf("FOLLOW(%c)={",nonterminals[i]);
+        for(int j=0;res[j]!='\0';j++){
+            printf(" %c",res[j]);
+        }
+        printf("}\n");
+    }
+}
